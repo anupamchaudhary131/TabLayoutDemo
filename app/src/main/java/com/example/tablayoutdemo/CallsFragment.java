70b7000ee7405a503d34d7284070a7d2892caca1
Anupam Chaudhary <anupamchaudhary131@gmail.com>
2022-07-14 17:55:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tablayoutdemo/CallsFragment.java
@@ -0,0 +1,25 @@
+package com.example.tablayoutdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CallsFragment extends Fragment {
+
+
+    public CallsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_calls, container, false);
+    }
+}
